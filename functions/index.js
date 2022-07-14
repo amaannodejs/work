98887204d5a9205565f7bb6e3c0176b326c5e547
Amaan Ansari <amaanansari@Amaans-MacBook-Pro.local>
2022-07-14 13:01:41 +0530

    firbase setup

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,23 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions"),
+      admin=require('firebase-admin'),
+      serviceAccount = require("../credentials/serviceAccountKey.json")
+      express=require('express'),
+      cors=require('cors'),
+      app=express(),
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+      });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(cors({origin:true}))
+
+
+app.get('/',(req,res)=>{
+    res.send('up')
+})
+
+
+
+
+
+
+exports.app=functions.https.onRequest(app);
